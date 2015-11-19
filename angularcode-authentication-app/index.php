4626d9b2ce5ed3db2a36a3ef74f6f6a2865464f8
bagaswidodo <bagaswidodo@gmail.com>
2015-11-19 09:10:32 +0700

    angular login

--- angularcode-authentication-app/index.php
+++ angularcode-authentication-app/index.php
@@ -19,9 +19,9 @@
                 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
                 <!--[if lt IE 9]><link href= "css/bootstrap-theme.css"rel= "stylesheet" >
 
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
+				<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+				<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+				<![endif]-->
               </head>
 
   <body ng-cloak="">
---
+++ myapp/grid.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html ng-app="myApp" ng-app lang="en">
+<head>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+    	ul>li, a{cursor: pointer;}
+    </style>
+    <title>Simple Datagrid with search, sort and paging using AngularJS, PHP, MySQL</title>
+</head>
+<div class="navbar navbar-default" id="navbar">
+    <div class="container" id="navbar-container">
+    <div class="navbar-header">
+        <a href="http://angularcode.com" class="navbar-brand">
+            <small>
+                <i class="glyphicon glyphicon-log-out"></i>
+                AngularCode / AngularJS Demos 
+            </small>
+        </a><!-- /.brand -->
+        
+    </div><!-- /.navbar-header -->
+    </div>
+</div>
+</html>
